a6c213f7e88a8f799eaba09393f1989ee8c269e0
matigobbi <matigobbi@gmail.com>
2022-11-30 11:40:20 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,64 +1,71 @@
-import { useState } from 'react'
+import { useState } from "react";
 import "./App.css";
 import contactsData from "./contacts.json";
 
-let firstInitialFive = contactsData.slice(0,5)
-
+let firstInitialFive = contactsData.slice(0, 5);
 
 function App() {
+  const [contacts] = useState(contactsData);
+  const [contactsToDisplay, setContatsToDisplay] = useState(firstInitialFive);
 
-  const [contacts] = useState(contactsData)
-  const [contactsToDisplay, setContatsToDisplay] = useState(firstInitialFive)
+  const addContact = () => {
+    const randomNumber = Math.floor(Math.random() * contactsData.length);
+    setContatsToDisplay([contacts[randomNumber], ...contactsToDisplay]);
+    console.log(contactsToDisplay);
+  };
 
-  const addContact= () => {
-    const randomNumber = Math.floor(Math.random() * contactsData.length)
-    console.log(contacts[randomNumber])
-    setContatsToDisplay([contacts[randomNumber], ...contactsToDisplay])
-    console.log(contactsToDisplay)
+  const sortPopularity = () => {
+    const sortedByPop = [...contactsToDisplay].sort((a, b) =>
+      a.popularity < b.popularity ? 1 : -1
+    );
+    setContatsToDisplay(sortedByPop);
+  };
 
-  }
-  const sortPopularity= ()=> {
-    const sortedByPop = [...contactsToDisplay].sort((a ,b)=> a.popularity< b.popularity ?  1 : -1)
-    setContatsToDisplay(sortedByPop)
-  }
-  const sortByName= () => {     
-    const sortedByName = [...contactsToDisplay].sort((a ,b)=> a.name > b.name ?  1 : -1)
-    setContatsToDisplay(sortedByName)
-  }
+  const sortByName = () => {
+    const sortedByName = [...contactsToDisplay].sort((a, b) =>
+      a.name > b.name ? 1 : -1
+    );
+    setContatsToDisplay(sortedByName);
+  };
 
-  const deleteContact=(contactId)=>{
-      const filteredContact = contactsToDisplay.filter(contact => contact.id !==contactId)
-      setContatsToDisplay(filteredContact)
-  }
+  const deleteContact = (contactId) => {
+    const filteredContact = contactsToDisplay.filter(
+      (contact) => contact.id !== contactId
+    );
+    setContatsToDisplay(filteredContact);
+  };
 
   return (
-  <>
-    <button onClick={addContact}>Add random contact</button>
-    <button onClick={sortPopularity}>Sort by pOpularity</button>
-    <button onClick={sortByName}>Sort by name</button>
-    <table className="table">
-      <tr>
-        <th>Image:</th>
-        <th>Name:</th>
-        <th>Popularity:</th>
-        <th>Won Oscar:</th>
-        <th>Won Emmy:</th>
-        <th>Actions:</th>
-      </tr>
-    {contactsToDisplay.map(contact => (
-      <tr>
-          <td><img className="img-contact" src={contact.pictureUrl} alt=""/></td>
-          <td> {contact.name}</td>
-          <td> {contact.popularity.toFixed(2)}</td>
-          <td> {contact.wonEmmy ? <p> 🎷</p> : <p>Nope </p>}</td>
-          <td> {contact.wonOscar ? <p> 🎷</p> : <p>Nope </p>}</td>
-          <td> <button onClick={() =>deleteContact(contact.id)}> Delete</button></td>
-      </tr>
-
-    ))}
-
-    </table>
-  </>
-  )
+    <>
+      <img src="./logo192.png" alt="asdads" />
+      <button onClick={addContact}>Add random contact</button>
+      <button onClick={sortPopularity}>Sort by pOpularity</button>
+      <button onClick={sortByName}>Sort by name</button>
+      <table className="table">
+        <tr>
+          <th>Image:</th>
+          <th>Name:</th>
+          <th>Popularity:</th>
+          <th>Won Oscar:</th>
+          <th>Won Emmy:</th>
+          <th>Actions:</th>
+        </tr>
+        {contactsToDisplay.map((contact) => (
+          <tr>
+            <td>
+              <img  className="img-contact" src={contact.pictureUrl} alt="" />
+            </td>
+            <td> {contact.name}</td>
+            <td> {contact.popularity.toFixed(2)}</td>
+            <td> {contact.wonEmmy ? <p> 🎷</p> : <p>Nope </p>}</td>
+            <td> {contact.wonOscar ? <p> 🎷</p> : <p>Nope </p>}</td>
+            <td>
+              <button onClick={() => deleteContact(contact.id)}>Delete</button>
+            </td>
+          </tr>
+        ))}
+      </table>
+    </>
+  );
 }
 export default App;
